6943c9ea37713a564be876f7d2cdad9314cabc4a
Steven Atkinson <steven.atkinson@gmx.com>
2016-11-09 14:33:29 +0000

    Created the Clock animation.

---
+++ lib/animations/clock-animation.rb
@@ -0,0 +1,30 @@
+class ClockAnimation
+  def frames
+    animation = (1..12).map do |i|
+      create_frame(1, clock(i))
+    end
+    animation.push(create_frame(1, "#{metal}#{angry}#{metal}"))
+    animation
+  end
+
+  private
+
+  def create_frame(delay, content)
+    AnimationFrame.new(content, {
+      delay: delay,
+      override_previous_frame: true
+    })
+  end
+
+  def clock(hour)
+    ":clock#{hour}:"
+  end
+
+  def metal
+    ':metal:'
+  end
+
+  def angry
+    ':angry:'
+  end
+end
