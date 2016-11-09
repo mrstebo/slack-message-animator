e16ce09bbdcf9fad0cc0466878b7a2be47a455f3
Steven Atkinson <steven.atkinson@gmx.com>
2016-11-09 09:28:30 +0000

    Added gifs to go with the quotes.

--- lib/animations/donald_trump_quotes_animation.rb
+++ lib/animations/donald_trump_quotes_animation.rb
@@ -8,7 +8,7 @@ class DonaldTrumpQuotesAnimation
 
   def frames
     (0..@length-1).map do |i|
-      create_frame(4, "Donald Trump: #{get_quote}")
+      create_frame(4, "#{get_trump_quote} - #{get_trump_gif}")
     end
   end
 
@@ -21,7 +21,7 @@ class DonaldTrumpQuotesAnimation
     })
   end
 
-  def get_quote
+  def get_trump_quote
     uri = URI('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -29,5 +29,20 @@ class DonaldTrumpQuotesAnimation
     response = http.get(uri.request_uri)
     content = JSON.parse(response.body)
     content['message']
+  end
+
+  def get_trump_gif
+    [
+      'http://i.giphy.com/10tkf2u5HFDGWk.gif',
+      'http://i.giphy.com/GHZ9RZFGqsWbK.gif',
+      'http://i.giphy.com/l46CnTiHrzuBnkAOA.gif',
+      'http://i.giphy.com/3o7TKWDGfvUWb9FMZi.gif',
+      'http://i.giphy.com/l2JhGYxcjMcKqiaHu.gif',
+      'http://i.giphy.com/3o7TKwiaIuMib5WVXO.gif',
+      'http://i.giphy.com/l2JhIUyUs8KDCCf3W.gif',
+      'http://i.giphy.com/l3vR3EssQ5ALagr7y.gif',
+      'http://i.giphy.com/3oriO6a2KKLMej1GyQ.gif',
+      'http://i.giphy.com/wJNGA01o1Zxp6.gif'
+    ].sample
   end
 end
