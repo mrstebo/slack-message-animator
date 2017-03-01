aa15df2610c1b0a1cc7c34b187006a906c2c5f42
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-03-01 09:27:33 +0000

    Added call to ask_questions.

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'highline/import'
 Dir["lib/**/*.rb"].each {|file| require_relative file }
 
 animations = [
-  {name: 'people', animation: PersonAnimation.new(['me'])},
+  {name: 'people', animation: PersonAnimation.new},
   {name: 'dog-car', animation: DogCarAnimation.new},
   {name: 'dog-car-punkd', animation: DogCarPunkdAnimation.new},
   {name: 'rolling-face', animation: RollingFaceAnimation.new},
@@ -21,10 +21,12 @@ animations = [
 selections = animations.each_with_index.map {|animation, index| "#{index + 1}: #{animation[:name]}"}.join("\n")
 animation = ask("What animation do you want to play?\n" + selections) {|q| q.echo = true}
 selected_animation = animations[animation.to_i - 1][:animation]
+selected_animation.ask_questions
 team_name = ask('What is your Slack team name?') {|q| q.echo = true}
 email = ask('What is your email address?') {|q| q.echo = true}
 password = ask('What is your password?') {|q| q.echo = '*'}
 channel = ask('What channel do you want to send a message to?') {|q| q.echo = true}
+
 session = SessionInitializer.init(team_name, email, password, channel)
 runner = AnimationRunner.new(session, selected_animation)
 
