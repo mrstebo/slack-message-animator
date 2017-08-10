a5cf2fd9574e350a37d9531c4d1b6527fa53bda2
Steve Atkinson <steve.atkinson@dootrix.com>
2017-08-10 13:05:33 +0100

    Fixed the AnimationRunner class.

--- lib/animation_runner.rb
+++ lib/animation_runner.rb
@@ -1,6 +1,5 @@
 class AnimationRunner
-  MESSAGE_INPUT_ELEMENT = '#msg_input'
-  MESSAGE_TEXT_ELEMENT = '#msg_text'
+  MESSAGE_INPUT_EDITOR = '.ql-editor'
 
   def initialize(session, animation)
     @session = session
@@ -8,7 +7,7 @@ class AnimationRunner
   end
 
   def animate
-    @session.assert_selector MESSAGE_INPUT_ELEMENT
+    @session.assert_selector MESSAGE_INPUT_EDITOR
     sleep 4
 
     frames.each_with_index do |frame, index|
@@ -29,19 +28,20 @@ class AnimationRunner
 
   def create_new_message(message)
     message.lines.each do |line|
-      @session.find(MESSAGE_INPUT_ELEMENT).native.send_keys line.chomp
-      @session.find(MESSAGE_INPUT_ELEMENT).native.send_keys [:control, :return]
+      @session.find(MESSAGE_INPUT_EDITOR).native.send_keys line.chomp
+      @session.find(MESSAGE_INPUT_EDITOR).native.send_keys [:control, :return]
     end
-    @session.find(MESSAGE_INPUT_ELEMENT).native.send_keys :return
+    @session.find(MESSAGE_INPUT_EDITOR).native.send_keys :return
   end
 
   def edit_previous_message(message)
-    @session.find(MESSAGE_INPUT_ELEMENT).native.send_keys :up
-    @session.find(MESSAGE_TEXT_ELEMENT).native.send_keys [:control, 'a']
+    @session.find(MESSAGE_INPUT_EDITOR).native.send_keys :up
+    @session.first(MESSAGE_INPUT_EDITOR).native.send_keys [:shift, :home]
+    @session.first(MESSAGE_INPUT_EDITOR).native.send_keys :backspace
     message.lines.each do |line|
-      @session.find(MESSAGE_TEXT_ELEMENT).native.send_keys line.chomp
-      @session.find(MESSAGE_TEXT_ELEMENT).native.send_keys [:control, :return]
+      @session.first(MESSAGE_INPUT_EDITOR).native.send_keys line.chomp
+      @session.first(MESSAGE_INPUT_EDITOR).native.send_keys [:control, :return]
     end
-    @session.find(MESSAGE_TEXT_ELEMENT).native.send_keys :return
+    @session.first(MESSAGE_INPUT_EDITOR).native.send_keys :return
   end
 end
